fd5c9970d5f37101c876120f857d393c75a95838
damenor <damenor.develop@gmail.com>
2024-08-26 22:32:59 +0200

    Add barril files, tailwind and prettier

---
+++ src/components/index.ts
@@ -0,0 +1,4 @@
+export { default as BaseHead } from './BaseHead.astro'
+export { default as Header } from './Header.astro'
+export { default as Footer } from './Footer.astro'
+export { default as FormattedDate } from './FormattedDate.astro'
