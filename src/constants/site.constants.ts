f04ded203e16e64a111a7229f6af65e9fb4283f4
c0d3hdz <marquezhernandez.damian@gmail.com>
2024-12-05 21:40:12 -0600

    contantes_site

--- src/constants/site.constants.ts
+++ src/constants/site.constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = '👨‍💻Iniciando en la Programación'
 export const SITE_DESCRIPTION = 'Blog de programación y desarrollo web'
-export const SITE_COPYRIGHT_AUTHOR = 'c0d3hdz'
-export const SITE_COPYRIGHT_LINK = ''
+export const SITE_COPYRIGHT_AUTHOR = 'c0d3hdz and MrCat-86'
+export const SITE_COPYRIGHT_LINK = 'https://github.com/c0d3hdz/Guia_Programacion'
