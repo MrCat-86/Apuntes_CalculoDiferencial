8acedebdb58cf9a286125c7dfe31a3cce2fd4147
MrCat-86 <mrcatgames.86@gmail.com>
2024-12-07 22:50:27 -0600

    Actualizacion de la Informacion del Index

--- src/constants/site.constants.ts
+++ src/constants/site.constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = '👨‍💻Iniciando en la Programación'
-export const SITE_DESCRIPTION = 'Blog de programación y desarrollo web'
-export const SITE_COPYRIGHT_AUTHOR = 'c0d3hdz and MrCat-86'
-export const SITE_COPYRIGHT_LINK = 'https://github.com/c0d3hdz/Guia_Programacion'
+export const SITE_TITLE = 'Inicios en el Calculo Diferencial'
+export const SITE_DESCRIPTION = 'Blog Sobre Calculo Diferencial Apegado al Programa del ITSH 2024'
+export const SITE_COPYRIGHT_AUTHOR = 'MrCat-86'
+export const SITE_COPYRIGHT_LINK = 'https://github.com/MrCat-86/Apuntes_CalculoDiferencial'
