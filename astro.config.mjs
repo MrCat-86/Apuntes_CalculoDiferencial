446b1b7ec865fe80924c5b78caeb842cb5ec8d5a
MrCat-86 <mrcatgames.86@gmail.com>
2024-12-08 23:22:27 -0600

    Actualizacion de Temas Hasta 1_5

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind()]
+  integrations: [
+    mdx({
+      extendMarkdownConfig: true, // Extiende la configuración para procesar KaTeX
+      rehypePlugins: [rehypeKatex], // Procesador de fórmulas matemáticas
+    }),
+    sitemap(),
+    tailwind(),
+  ],
 });
--- src/constants/site.constants.ts
+++ src/constants/site.constants.ts
@@ -1,4 +1,4 @@
-export const SITE_TITLE = 'Inicios en el Calculo Diferencial'
+export const SITE_TITLE = '🤓☝️ Inicios en el Calculo Diferencial 🤔🔢'
 export const SITE_DESCRIPTION = 'Blog Sobre Calculo Diferencial Apegado al Programa del ITSH 2024'
 export const SITE_COPYRIGHT_AUTHOR = 'MrCat-86'
 export const SITE_COPYRIGHT_LINK = 'https://github.com/MrCat-86/Apuntes_CalculoDiferencial'
