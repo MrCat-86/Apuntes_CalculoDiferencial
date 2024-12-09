89eca1464051d6021e3c330231d4e0b0f488c19c
MrCat-86 <mrcatgames.86@gmail.com>
2024-12-08 23:29:42 -0600

    Correccion astroconfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,11 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from '@astrojs/tailwind';
-import rehypeKatex from 'rehype-katex';
+
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [
-    mdx({
-      extendMarkdownConfig: true, // Extiende la configuración para procesar KaTeX
-      rehypePlugins: [rehypeKatex], // Procesador de fórmulas matemáticas
-    }),
-    sitemap(),
-    tailwind(),
-  ],
+  integrations: [mdx(), sitemap(), tailwind()]
 });
